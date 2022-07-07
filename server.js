06c755945218cf3671bcd6b27662ac70e29f00f8
Aluminé Vila <alumine.vk@gmail.com>
2022-07-07 20:56:49 -0300

    actualizo todo

--- server.js
+++ server.js
@@ -1,68 +1,87 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const config = require("./config.json");
+
+let prefix = config.prefix
 
 client.on("ready", () => {
-    console.log("Estoy listo!");
- });
- 
- client.on("message", (message) => {
-   if(message.content.startsWith(".ladis")) {
-     message.channel.send("k onda perris");
-   } 
-   if(message.content.startsWith(".sorteo")) {
-       message.channel.send("toy chikita pero te dejo una página https://www.sortea2.com/sorteos");
-   }
-   // acá empieza enviar a otros canales
-   if(message.content.startsWith(".pelis")) {
-     let texto = message.content.slice(7)
+  console.log("listo!");
+});
 
-     client.channels.resolve("703845151671320638").send(texto);
-   }
-   if(message.content.startsWith(".general")) {
-    let texto = message.content.slice(9)
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
 
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  if (command === 'ladis') {
+    message.channel.send("k onda perris");
+  } else
+  if (command === 'comandos') {
+    message.channel.send("Esto es lo que tenemos hasta ahora: | .ladis | .comandos | .sorteo | Mandar mensaje a otro canal: .general .musikita .pelis .series .recetas .bandas-artistas .videosyt .jueguitos .cursos .costura | .compatibilidad fulanito pepito |");
+  } else
+
+  if (command === 'compatibilidad') {
+    let pepito = args[0];
+    let fulanito = args[1];
+    let numRandom = Math.floor(Math.random() * 99);
+
+    message.channel.send(`La compatibilidad entre ${pepito} y ${fulanito} es de ${numRandom}%`);
+  } else
+
+  if (command === 'sorteo') {
+    let rand = Math.floor(Math.random()*args.length);
+    let rValue = args[rand];
+
+    message.channel.send(rValue);
+  } else 
+
+  
+  if (command === 'general') {
+    let texto = args.join(" ");
     client.channels.resolve("806945431724359701").send(texto);
-  }
-  if(message.content.startsWith(".musikita")) {
-    let texto = message.content.slice(10)
-
+  } else
+  if (command === 'musikita') {
+    let texto = args.join(" ");
     client.channels.resolve("886441715219714048").send(texto);
-  }
-  if(message.content.startsWith(".series")) {
-    let texto = message.content.slice(8)
-
+  } else
+  if (command === 'pelis') {
+    let texto = args.join(" ");
+    client.channels.resolve("703845151671320638").send(texto);
+  } else
+  if (command === 'series') {
+    let texto = args.join(" ");
     client.channels.resolve("750125792729497651").send(texto);
-  }
-  if(message.content.startsWith(".recetas")) {
-    let texto = message.content.slice(9)
-
+  } else
+  if (command === 'recetas') {
+    let texto = args.join(" ");
     client.channels.resolve("749071129087443074").send(texto);
-  }
-  if(message.content.startsWith(".bandas")) {
-    let texto = message.content.slice(8)
-
+  } else
+  if (command === 'bandas-artistas') {
+    let texto = args.join(" ");
     client.channels.resolve("771941146929791018").send(texto);
-  }
-  if(message.content.startsWith(".videosyt")) {
-    let texto = message.content.slice(10)
-
+  } else
+  if (command === 'videosyt') {
+    let texto = args.join(" ");
     client.channels.resolve("793894202991443968").send(texto);
-  }
-  if(message.content.startsWith(".cursos")) {
-    let texto = message.content.slice(8)
-
+  } else
+  if (command === 'jueguitos') {
+    let texto = args.join(" ");
+    client.channels.resolve("857085467002535986").send(texto);
+  } else
+  if (command === 'cursos') {
+    let texto = args.join(" ");
     client.channels.resolve("880250115837804544").send(texto);
-  }
-  if(message.content.startsWith(".costura")) {
-    let texto = message.content.slice(9)
-
+  } else
+  if (command === 'costura') {
+    let texto = args.join(" ");
     client.channels.resolve("891877433505112076").send(texto);
   }
-  //listo (?)
-  if(message.content.startsWith(".comandos")) {
-    message.channel.send("Esto es lo que tenemos hasta ahora: | .ladis | .comandos | .sorteo | Mandar mensaje a otro canal: .general .musikita .pelis .series .recetas .bandas .videosyt .cursos .costura |");
-  }
- });
+});
 
- 
- client.login("OTgyODM3NzAxNTE2NTkxMTg0.GMFM2h.Ng9_1idknogbx9SPYTEuOFVBr419jUssAvijcI");
+
+
+
+client.login(config.token);
